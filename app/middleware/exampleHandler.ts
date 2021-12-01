b9aaa8cac520cabf0ab84c21c57b030b9532bcf2
legends-killer <yyy@legends-killer.cq.cn>
2021-12-01 21:46:11 +0800

    fix: example post proxy

--- app/middleware/exampleHandler.ts
+++ app/middleware/exampleHandler.ts
@@ -2,7 +2,7 @@
  * @Author: legends-killer
  * @Date: 2021-12-01 15:11:08
  * @LastEditors: legends-killer
- * @LastEditTime: 2021-12-01 21:18:25
+ * @LastEditTime: 2021-12-01 21:45:36
  * @Description:
  */
 import { Context } from 'egg'
@@ -15,7 +15,9 @@ export default () => {
       ctx.throw(400, '这只是个example捏，不能修改捏', { code: 40000 })
     }
     if (method === 'POST') {
-      if (url.includes('/system/log') || url.includes('/auth')) {
+      const reg = /^\/api.*$/
+      const test = reg.test(url)
+      if (url.includes('/system/log') || url.includes('/auth') || test) {
         // nya
       } else if (url.includes('/system/reload')) {
         ctx.throw(400, '这只是个example捏，不能重启捏', { code: 40000 })
